2a0dc8cb313075995647aed71ef4b519b7ed1461
Anna Zocher <anna.zocher@gmail.com>
2021-06-25 13:57:28 -0700

    solution pre-bug check

--- src/Clock.js
+++ src/Clock.js
@@ -4,13 +4,41 @@ export default function Clock() {
    const [secs, setSecs] = useState(0)
    const [mins, setMins] = useState(0)
    const [hours, setHours] = useState(0)
+   const [intervalHolder, setIntervalHolder] = useState(null)
+
+   const toDegrees = (increment, step) => {
+        return (increment / step) * 360
+    }
+
+    const clockRun = (secondHand, minuteHand, hourHand) => {
+        setSecs(secs + 1)
+        const secDegrees = toDegrees(secs, 60)
+        secondHand.style.transform = `rotate(${secDegrees}deg)`
+        if((secs % 60) === 0){
+          setMins(mins + 1)
+          const minDegrees = toDegrees(mins, 60)
+          minuteHand.style.transform = `rotate(${minDegrees}deg)`
+        }
+        if((secs % 3600) === 0){
+          setHours(hours + 1)
+          const hourDegrees = toDegrees(hours, 12)
+          hourHand.style.transform = `rotate(${hourDegrees}deg)`
+        }
+      }
+
+    const stopTimer = (timer) => {
+        return clearInterval(timer)
+    }
 
     useEffect(() => {
         const secondHand = document.getElementById("second")
         const minuteHand = document.getElementById("minute")
         const hourHand = document.getElementById("hour")
 
-        
+        let intervalRun = setInterval(clockRun(secondHand, minuteHand, hourHand), 1000)
+        setIntervalHolder(intervalRun)
+        clearInterval(intervalHolder)
+
     }, [])
 
    return (
