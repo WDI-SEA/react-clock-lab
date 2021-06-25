35dfe7fd7cc0405133df930fde7abbf9ba17d786
Anna Zocher <anna.zocher@gmail.com>
2021-06-25 13:39:42 -0700

    starting wb-solve version solution

--- src/Clock.js
+++ src/Clock.js
@@ -1,8 +1,17 @@
 import { useState, useEffect } from "react"
 
 export default function Clock() {
-   
-    return (
- 
+   const [secs, setSecs] = useState(0)
+   const [mins, setMins] = useState(0)
+   const [hours, setHours] = useState(0)
+
+
+   return (
+    <div id="clock">
+        <img id="face" src="img/clockface.png"/>
+        <img id="second" src="img/second-hand.png"/>
+        <img id="minute" src="img/minute-hand.png"/>
+        <img id="hour" src="img/hour-hand.png"/>
+    </div>
     )
 }
